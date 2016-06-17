22db2531e80719f05110779484c86df69b9cb35c
Sean Burke <sburke@globalfit.com>
2016-06-17 13:23:54 -0400

    more vagrant aliases

--- vagrant.zsh
+++ vagrant.zsh
@@ -1,15 +1,32 @@
 #Vagrant
 alias v='vagrant'
 
+#Init
+alias vint='vagrant init'
+
+#Up
 alias vup='vagrant up'
 alias vu='vagrant up'
 
+#Status
 alias vst='vagrant status'
 alias vstatus='vagrant status'
 alias vs='vagrant status'
 
+#Halt
 alias vhalt='vagrant halt'
 alias vh='vagrant halt'
 
+#Destroy
 alias vdest='vagrant destroy'
-alias vd='vagrant destrup'
+alias vd='vagrant destroy'
+
+#Boxes
+alias vb='vagrant box'
+alias vbu='vagrant box update'
+alias vbl='vagrant box list'
+
+#Plugins
+alias vp='vagrant plugin'
+alias vpl='vagrant plugin list'
+
