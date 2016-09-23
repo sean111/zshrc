1d865012b5b39f5b706c8d3cb4169dd89f8ba11a
Sean Burke <sburke@globalfit.com>
2016-09-23 12:29:58 -0400

    Started adding some docker commands

---
+++ docker.zsh
@@ -0,0 +1,14 @@
+#Docker
+alias d='docker'
+
+alias dup='docker up'
+
+alias dk='docker kill'
+alias dkill='dk'
+
+#Docker Compose
+alias dc='docker-compose'
+
+alias dcu='dc up'
+alias dck='dc kill'
+alias dcb='dc build
