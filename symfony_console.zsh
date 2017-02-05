0352b4f9163ac6478a0afb769a2db658e44be2fe
Sean Burke <sean111@gmail.com>
2017-02-05 11:38:45 -0500

    some symfony zsh updates

--- symfony_console.zsh
+++ symfony_console.zsh
@@ -1,5 +1,21 @@
+_symfony_console () {
+  echo "php $(find . -maxdepth 2 -mindepth 1 -name 'console' -type f | head -n 1)"
+}
+
+_symfony2_get_command_list () {
+   `_symfony_console` --no-ansi | sed "1,/Available commands/d" | awk '/^  ?[^ ]+ / { print $1 }'
+}
+
+_symfony2 () {
+   compadd `_symfony2_get_command_list`
+}
+
+compdef _symfony2 '`_symfony_console`'
+compdef _symfony2 'bin/console'
+compdef _symfony2 sf
+
 #Symfony Console
-alias scon='php bin/console'
+alias scon='`_symfony_console`'
 
 #Cache
 alias sconcc='scon cache:clear'
@@ -12,3 +28,6 @@ alias scondf='scon doctrine:schema:update --force'
 alias sconai='scon asset:install'
 alias sconad='scon assetic:dump'
 alias sconaw='scon assetic watch'
+
+alias sconp="scon --env=prod"
+alias scond="scon --env=dev"
